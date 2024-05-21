d7500ec1b37c772bdf8447619e402db7dd789a9c
Henrik Nelson <henrik.nelson@verisure.com>
2024-05-21 11:46:29 +0200

    new completions

---
+++ aliases/kcdp.aliases.bash
@@ -0,0 +1,6 @@
+# shellcheck shell=bash
+about-alias 'kcdp aliases'
+
+#if _command_exists kcdd; then
+alias kcdp='kubectl describe pods'
+#fi
---
+++ completion/kcdd.completion.bash
@@ -0,0 +1,19 @@
+_complete_kcdd () {
+
+  local curr_arg;
+  curr_arg=${COMP_WORDS[COMP_CWORD]}
+
+  case $COMP_CWORD in
+
+    1)
+      COMPREPLY=($(compgen -W "$(kubectl get deployments.apps -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')" -- $curr_arg ))
+      ;;
+
+    **)
+      COMPREPLY=()
+      ;;
+
+  esac
+}
+
+complete -F _complete_kcdd kcdd
